8052caf45977aa90db7777e0602831a4244548f1
Екатерина <polskaya007@mail.ru>
2023-06-10 20:52:34 +0300

    correcton function

--- src/orderByProps.js
+++ src/orderByProps.js
@@ -1,14 +1,23 @@
-export default function orderByProps(obj, [prop1,prop2]){
-  const arr = [];
-  let sorting = [];
-  for (const prop in obj) {
-    if (prop === prop1 || prop === prop2) {
-      arr.push(`${prop} : ${obj[prop]}`);
+export default function orderByProps(obj, orderArray) {
+  const primaryProps = [];
+  const sideProps = [];
+
+  for (const property in obj) {
+    if (orderArray.includes(property)) {
+      primaryProps[orderArray.findIndex((item) => item === property)] = {
+        key: property, value: obj[property],
+      };
     } else {
-      sorting.push(`${prop} : ${obj[prop]}`);
-      sorting.sort();
+      sideProps.push({ key: property, value: obj[property] });
     }
   }
-  const result = [...arr, ...sorting];
-  return result;
+
+  sideProps.sort((a, b) => {
+    if (a.key < b.key) {
+      return -1;
+    }
+    return 0;
+  });
+
+  return primaryProps.concat(sideProps);
 }
